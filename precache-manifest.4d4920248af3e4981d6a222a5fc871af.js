376c5b81f2e6101ac95cbd07753106aeb9344c62
Eduardo Araújo <eduardoaraujo@MBP-de-Eduardo.minder.aveiro.mindera.com>
2020-02-10 11:39:33 +0000

    Updates

---
+++ precache-manifest.4d4920248af3e4981d6a222a5fc871af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dccbd784bc7702131728a3036ffacb00",
+    "url": "/pagetransitions/index.html"
+  },
+  {
+    "revision": "a8e360bea7769759f4aa",
+    "url": "/pagetransitions/static/css/main.394cc68e.chunk.css"
+  },
+  {
+    "revision": "08ee2db683d726e19edf",
+    "url": "/pagetransitions/static/js/2.b9bee07f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pagetransitions/static/js/2.b9bee07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e360bea7769759f4aa",
+    "url": "/pagetransitions/static/js/main.406f0bc4.chunk.js"
+  },
+  {
+    "revision": "647006456617e4b16e37",
+    "url": "/pagetransitions/static/js/runtime-main.93c5a7da.js"
+  }
+]);
