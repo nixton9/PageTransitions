a2cef125f83330cde5604fab209431ae36841d5a
Eduardo Araújo <eduardoaraujo@MBP-de-Eduardo.minder.aveiro.mindera.com>
2020-02-10 11:52:36 +0000

    Router fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router} from 'react-router-dom';
 
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
